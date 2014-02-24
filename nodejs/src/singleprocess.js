fe259f528399136ca0adc39df5168f7539cc6542
blade pan <panxiaozhong@gmail.com>
2014-02-24 00:51:55 -0500

    init nodes server source

---
+++ nodejs/src/singleprocess.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+//[0] is node [1] is script name
+var portNum = parseInt(process.argv[2]);
+var dir = process.argv[3];
+
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(dir, unescape(uri));
+    res.writeHead(200, {
+                'Content-Type': 'audio/example'
+            });
+    fs.createReadStream(filename)
+                        .pipe(res);
+}).listen(portNum, '127.0.0.1');
+console.log('Server ' + process.pid +' running at port '+portNum);
